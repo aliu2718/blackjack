c11cb3f5cdba1f5fb564038730cf6fce855df762
Jason Steiner <jns226@cornell.edu>
2022-10-19 09:52:41 -0400

    Initialize test suite
    - Added test cases for string_of_card and parse respectively

--- test/main.ml
+++ test/main.ml
@@ -2,6 +2,38 @@
 
 open OUnit2
 open Blackjack
+open Card
+open Command
+open Deck
 
-let suite = "Test Suite for Blackjack System" >::: List.flatten []
+(** [string_of_card_test name c expected_output] constructs an OUnit test named
+    [name] that asserts the quality of [expected_output] with
+    [string_of_card c]. *)
+let string_of_card_test (name : string) (c : Card.t) (expected_output : string)
+    : test =
+  name >:: fun _ ->
+  assert_equal expected_output (string_of_card c) ~printer:Fun.id
+
+let test_card_1 = init_card Ace Diamonds [ 1; 11 ]
+
+let card_tests =
+  [
+    (* string_of_card_test *)
+    string_of_card_test "String of test_card_1" test_card_1
+      "(Ace, Diamonds, [1; 11])";
+  ]
+
+let command_tests =
+  [
+    "parse test suite"
+    >::: [
+           ( "    bet   500    is Bet 500" >:: fun _ ->
+             assert_equal (Bet 500) (parse "    bet   500   ") );
+           ("hit is Hit" >:: fun _ -> assert_equal Hit (parse "hit"));
+         ];
+  ]
+
+let suite =
+  "test suite for final project" >::: List.flatten [ card_tests; command_tests ]
+
 let _ = run_test_tt_main suite
