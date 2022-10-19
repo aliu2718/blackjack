4d43c9c109cbb452088dcfee872543c188296b10
Lincoln Stranger <las535@cornell.edu>
2022-10-19 11:48:26 -0400

    Implement Deck.standard, Deck.size
    - Implemented Deck.standard
    - Implemented Deck.size
    - Redesigned Deck test suite

--- src/deck.ml
+++ src/deck.ml
@@ -3,8 +3,64 @@ open Card
 type t = Card.t list
 
 let empty : t = []
-let standard = []
-let size d = raise (Failure "Unimplemented: Deck.size")
+
+let standard : t =
+  [
+    init_card Ace Spades [ 1; 11 ];
+    init_card (Number 2) Spades [ 2 ];
+    init_card (Number 3) Spades [ 3 ];
+    init_card (Number 4) Spades [ 4 ];
+    init_card (Number 5) Spades [ 5 ];
+    init_card (Number 6) Spades [ 6 ];
+    init_card (Number 7) Spades [ 7 ];
+    init_card (Number 8) Spades [ 8 ];
+    init_card (Number 9) Spades [ 9 ];
+    init_card (Number 10) Spades [ 10 ];
+    init_card Jack Spades [ 10 ];
+    init_card Queen Spades [ 10 ];
+    init_card King Spades [ 10 ];
+    init_card Ace Hearts [ 1; 11 ];
+    init_card (Number 2) Hearts [ 2 ];
+    init_card (Number 3) Hearts [ 3 ];
+    init_card (Number 4) Hearts [ 4 ];
+    init_card (Number 5) Hearts [ 5 ];
+    init_card (Number 6) Hearts [ 6 ];
+    init_card (Number 7) Hearts [ 7 ];
+    init_card (Number 8) Hearts [ 8 ];
+    init_card (Number 9) Hearts [ 9 ];
+    init_card (Number 10) Hearts [ 10 ];
+    init_card Jack Hearts [ 10 ];
+    init_card Queen Hearts [ 10 ];
+    init_card King Hearts [ 10 ];
+    init_card Ace Clubs [ 1; 11 ];
+    init_card (Number 2) Clubs [ 2 ];
+    init_card (Number 3) Clubs [ 3 ];
+    init_card (Number 4) Clubs [ 4 ];
+    init_card (Number 5) Clubs [ 5 ];
+    init_card (Number 6) Clubs [ 6 ];
+    init_card (Number 7) Clubs [ 7 ];
+    init_card (Number 8) Clubs [ 8 ];
+    init_card (Number 9) Clubs [ 9 ];
+    init_card (Number 10) Clubs [ 10 ];
+    init_card Jack Clubs [ 10 ];
+    init_card Queen Clubs [ 10 ];
+    init_card King Clubs [ 10 ];
+    init_card Ace Diamonds [ 1; 11 ];
+    init_card (Number 2) Diamonds [ 2 ];
+    init_card (Number 3) Diamonds [ 3 ];
+    init_card (Number 4) Diamonds [ 4 ];
+    init_card (Number 5) Diamonds [ 5 ];
+    init_card (Number 6) Diamonds [ 6 ];
+    init_card (Number 7) Diamonds [ 7 ];
+    init_card (Number 8) Diamonds [ 8 ];
+    init_card (Number 9) Diamonds [ 9 ];
+    init_card (Number 10) Diamonds [ 10 ];
+    init_card Jack Diamonds [ 10 ];
+    init_card Queen Diamonds [ 10 ];
+    init_card King Diamonds [ 10 ];
+  ]
+
+let size (d : t) : int = List.length d
 let add d c = raise (Failure "Unimplemented: Deck.add")
 let combine d1 d2 = raise (Failure "Unimplemented: Deck.combine")
 let shuffle d = raise (Failure "Unimplemented: Deck.shuffle")
@@ -17,4 +73,4 @@ let draw d n = raise (Failure "Unimplemented: Deck.draw")
 let rec string_of_deck d =
   match d with
   | [] -> ""
-  | h :: t -> Card.string_of_card h ^ string_of_deck t
+  | h :: t -> Card.string_of_card h ^ " " ^ string_of_deck t
--- test/main.ml
+++ test/main.ml
@@ -14,11 +14,17 @@ let string_of_card_test (name : string) (c : Card.t) (expected_output : string)
   name >:: fun _ ->
   assert_equal expected_output (string_of_card c) ~printer:Fun.id
 
-let empty_test (name : string) (expected_output : Deck.t) : test =
-  name >:: fun _ -> assert_equal expected_output empty ~printer:string_of_deck
+let string_of_deck_test (name : string) (d : Deck.t) (expected_output : string)
+    : test =
+  name >:: fun _ ->
+  assert_equal expected_output (string_of_deck d) ~printer:Fun.id
+
+let size_test (name : string) (d : Deck.t) (expected_output : int) : test =
+  name >:: fun _ -> assert_equal expected_output (size d) ~printer:string_of_int
 
 let test_card_1 = init_card Ace Diamonds [ 1; 11 ]
 let test_empty = empty
+let test_standard = standard
 
 let card_tests =
   [
@@ -37,7 +43,31 @@ let command_tests =
          ];
   ]
 
-let deck_tests = [ empty_test "Empty constructor test" test_empty ]
+let deck_tests =
+  [
+    string_of_deck_test "empty val test" test_empty "";
+    string_of_deck_test "standard val test" test_standard
+      "(Ace, Spades, [1; 11]) (Number 2, Spades, [2]) (Number 3, Spades, [3]) \
+       (Number 4, Spades, [4]) (Number 5, Spades, [5]) (Number 6, Spades, [6]) \
+       (Number 7, Spades, [7]) (Number 8, Spades, [8]) (Number 9, Spades, [9]) \
+       (Number 10, Spades, [10]) (Jack, Spades, [10]) (Queen, Spades, [10]) \
+       (King, Spades, [10]) (Ace, Hearts, [1; 11]) (Number 2, Hearts, [2]) \
+       (Number 3, Hearts, [3]) (Number 4, Hearts, [4]) (Number 5, Hearts, [5]) \
+       (Number 6, Hearts, [6]) (Number 7, Hearts, [7]) (Number 8, Hearts, [8]) \
+       (Number 9, Hearts, [9]) (Number 10, Hearts, [10]) (Jack, Hearts, [10]) \
+       (Queen, Hearts, [10]) (King, Hearts, [10]) (Ace, Clubs, [1; 11]) \
+       (Number 2, Clubs, [2]) (Number 3, Clubs, [3]) (Number 4, Clubs, [4]) \
+       (Number 5, Clubs, [5]) (Number 6, Clubs, [6]) (Number 7, Clubs, [7]) \
+       (Number 8, Clubs, [8]) (Number 9, Clubs, [9]) (Number 10, Clubs, [10]) \
+       (Jack, Clubs, [10]) (Queen, Clubs, [10]) (King, Clubs, [10]) (Ace, \
+       Diamonds, [1; 11]) (Number 2, Diamonds, [2]) (Number 3, Diamonds, [3]) \
+       (Number 4, Diamonds, [4]) (Number 5, Diamonds, [5]) (Number 6, \
+       Diamonds, [6]) (Number 7, Diamonds, [7]) (Number 8, Diamonds, [8]) \
+       (Number 9, Diamonds, [9]) (Number 10, Diamonds, [10]) (Jack, Diamonds, \
+       [10]) (Queen, Diamonds, [10]) (King, Diamonds, [10])";
+    size_test "size of empty is 0" test_empty 0;
+    size_test "size of standard is 52" test_standard 52;
+  ]
 
 let suite =
   "test suite for final project"
