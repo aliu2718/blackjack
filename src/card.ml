ea4f375492c77fc8cc3fd0068dbc424e8d77ed5f
Jason Steiner <jns226@cornell.edu>
2022-10-19 09:49:22 -0400

    Implement Card.string_of_card
    - Three helper functions
    - Formats string like a tuple

--- src/card.ml
+++ src/card.ml
@@ -21,4 +21,24 @@ let init_card r s v = { rank = r; suit = s; values = v }
 let rank c = c.rank
 let suit c = c.suit
 let values c = c.values
-let string_of_card c = raise (Failure "Unimplemented: Card.string_of_card")
+
+let string_of_rank = function
+  | Ace -> "Ace"
+  | Number int -> "Number " ^ string_of_int int
+  | Jack -> "Jack"
+  | Queen -> "Queen"
+  | King -> "King"
+
+let string_of_suit = function
+  | Diamonds -> "Diamonds"
+  | Hearts -> "Hearts"
+  | Spades -> "Spades"
+  | Clubs -> "Clubs"
+
+let rec string_of_int_list values =
+  "[" ^ String.concat "; " (List.map string_of_int values) ^ "]"
+
+let string_of_card c =
+  "(" ^ string_of_rank c.rank ^ ", " ^ string_of_suit c.suit ^ ", "
+  ^ string_of_int_list c.values
+  ^ ")"
