ac7ff88004b9047ffda70172ab29df95df01aa10
Andrew Liu <al849@cornell.edu>
2022-10-21 08:42:03 -0400

    Modify state implementation
    - Removed tracking of the current hand being played
    - Modified State.current_hand to return the appropriate hand given the current turn

--- src/state.ml
+++ src/state.ml
@@ -13,7 +13,6 @@ type t = {
   dealer_hand : h;
   player_hands : h * h;
   curr_turn : turn;
-  curr_hand : h;
 }
 
 exception IllegalAction
@@ -26,7 +25,6 @@ let init_state =
     dealer_hand = empty_hand;
     player_hands = (empty_hand, empty_hand);
     curr_turn = Player;
-    curr_hand = empty_hand;
   }
 
 let add_deck st d = { st with deck = d |> combine st.deck |> shuffle }
@@ -52,7 +50,6 @@ let rec start_round st =
       deck = new_deck;
       dealer_hand = [ dealer_card ];
       player_hands = (new_player_hand, empty_hand);
-      curr_hand = new_player_hand;
     }
   with EmptyDeck -> start_round (add_deck st standard)
 
@@ -71,7 +68,13 @@ let bet st n = raise (Failure "Unimplemented: State.bet")
 let deposit st n = raise (Failure "Unimplemented: State.deposit")
 let current_bet st = raise (Failure "Unimplemented: State.current_bet")
 let current_turn st = st.curr_turn
-let current_hand st = st.curr_hand
+
+let current_hand st =
+  match st.curr_turn with
+  | Dealer -> st.dealer_hand
+  | Player -> fst st.player_hands
+  | PlayerSplit -> snd st.player_hands
+
 let player_hands st = st.player_hands
 let dealer_hand st = st.dealer_hand
 let hit st = raise (Failure "Unimplemented: State.hit")
